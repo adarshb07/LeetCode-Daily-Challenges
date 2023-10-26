1518f741bf0f3e727ca65102d85f083f0b7fd41d
Adarsh Bhardwaj <55309341+adarshb07@users.noreply.github.com>
2023-10-26 21:39:33 +0530

    Time: 0 ms (100%), Space: 6.2 MB (51.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if( x < 0 || x != 0 && x % 10 == 0)
+        if( x < 0 || (x != 0 && x % 10 == 0 )){
             return 0;
-        int r = 0;
-        while(x > r){
-            r = (r * 10) + (x%10);
-            x /=10;
+        }      
+        int rev = 0;
+        while(x > rev){
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
         }
-        return x == r || x == r / 10;
+        return (x ==  rev ||  x == rev / 10);
     }
 };
